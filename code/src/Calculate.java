51496c182ab0f98eb8e09f35c2a28d6f85b61133
TusharVerma25 <tusharv457@gmail.com>
2022-02-10 09:39:58 +0530

    calcultor made

--- code/src/Calculate.java
+++ code/src/Calculate.java
@@ -1,9 +1,9 @@
-class Calculate{
+class Calculator{
 
     private int op1;
     private int op2;
 
-    public Calculate(int op1, int op2) {
+    public Calculator(int op1, int op2) {
         this.op1 = op1;
         this.op2 = op2;
     }
@@ -36,19 +36,24 @@ class Calculate{
         return this.getOperand1()*this.getOperand2();
     }
 
-    public int divide(){
-        return this.getOperand1()/this.getOperand2();
+    public double divide(){
+        return this.getOperand1()*1.0/this.getOperand2();
+    }
+
+    public String toString() {
+        return "Calculator";
     }
 }
 
-public class Calculator {
+public class Calculate {
 
     public static void main(String[] args) {
 
-        Calculate calculate= new Calculate(10,2);
-        System.out.println(calculate.add());
-        System.out.println(calculate.subtract());
-        System.out.println(calculate.multiply());
-        System.out.println(calculate.divide());
+        Calculator calculator= new Calculator(10,4);
+        System.out.println(calculator.add());
+        System.out.println(calculator.subtract());
+        System.out.println(calculator.multiply());
+        System.out.println(calculator.divide());
+        System.out.println(calculator);
     }
 }
