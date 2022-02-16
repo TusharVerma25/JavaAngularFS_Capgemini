896df2cd76f20537d8e4e89cd766ac51e66ee449
TusharVerma25 <tusharv457@gmail.com>
2022-02-16 17:02:48 +0530

    commit

---
+++ code/src/com/mysite/ExceptionalHandling.java
@@ -0,0 +1,48 @@
+package com.mysite;
+
+class InsufficientAmountException extends RuntimeException
+{
+    InsufficientAmountException()
+    {
+        System.out.println("Withdrawal amount is greater than your current balance.");
+    }
+}
+
+class Account {
+
+    double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdrawAmount(double amt) throws InsufficientAmountException {
+        try {
+            if(amt>this.balance) {
+                throw new InsufficientAmountException();
+            }
+            this.balance= this.balance-amt;
+            System.out.println("Amount withdrawn " + amt + "\nAvailabe balance " + this.balance);
+
+        }catch(InsufficientAmountException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+public class ExceptionalHandling {
+    public static void main(String[] args) {
+
+        Account account = new Account(5000);
+        account.withdrawAmount(8000);
+        account.withdrawAmount(1000);
+    }
+}
